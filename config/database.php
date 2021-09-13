fd9e209c6bdc228da7d79c0c066568998c11afc0
Karel van Zijl <sonoind@gmail.com>
2021-09-13 12:06:05 +0800

    [TEST] undo read & write database

--- config/database.php
+++ config/database.php
@@ -44,17 +44,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'read' => [
-                'host' => [
-                    env('DB_HOST_READ', '127.0.0.1'),
-                ],
-            ],
-            'write' => [
-                'host' => [
-                    env('DB_HOST', '127.0.0.1'),
-                ],
-            ],
-            // 'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
